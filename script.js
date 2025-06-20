3cf66afdb7be63ef1331599ed3f7fdc539b7fa3c
InsanoerZaki Faiq Istighfar <insanoerzaki@gmail.com>
2025-06-20 20:54:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -130,6 +130,7 @@ async function addTransaction(e) {
   document.getElementById("amount").value = "";
   document.getElementById("type").value = "Jenis";
   document.getElementById("category").value = "Semua";
+  document.getElementById("filterCategory").value = "Kategori";
 
   updateBalanceDisplay();
   renderTransactions();
