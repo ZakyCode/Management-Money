71b06e2b0842a81c20e85c88fb4714035cec511b
InsanoerZaki Faiq Istighfar <insanoerzaki@gmail.com>
2025-06-21 19:46:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -350,6 +350,17 @@ document.addEventListener("DOMContentLoaded", () => {
   logoutBtn.addEventListener("click", handleLogout);
 });
 
+// Ganti bagian toggle event listener
+togglePassword.addEventListener('click', function() {
+  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', type);
+  
+  // Toggle icon
+  const icon = this.querySelector('i');
+  icon.classList.toggle('fa-eye');
+  icon.classList.toggle('fa-eye-slash');
+});
+
 // Fungsi global untuk delete
 window.deleteTransaction = deleteTransaction;
 
